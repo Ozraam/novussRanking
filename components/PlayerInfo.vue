0a7616727e89fce23aa8070dce6f59fcbe4d14f1
ARDOUIN Tristan <tristan.ardouin3@etu.univ-lorraine.fr>
2023-10-19 15:49:39 +0300

    ✨Player are displayed in elo order

---
+++ components/PlayerInfo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+    player: {
+        type: Object as PropType<{id: number, name: string, elo:number}>,
+        required: true
+    },
+    rank: {
+        type: Number,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <li class="player">
+        <div class="player-name">
+            {{ rank }} {{ player.name }}
+        </div>
+
+        <div class="player-elo">
+            {{ player.elo }}
+        </div>
+    </li>
+</template>
---
+++ components/PlayerList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+    players: {
+        type: Array as PropType<{id: number, name: string, elo:number}[] | null>,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>Players</h1>
+
+        <ul>
+            <player-info
+                v-for="(player, index) in players"
+                :key="player.id"
+                :player="player"
+                :rank="index + 1"
+            />
+        </ul>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.player {
+  display: flex;
+  gap: 10px;
+}
+</style>
